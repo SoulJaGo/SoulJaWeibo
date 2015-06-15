fee9b55efa6a801552b2a5f7a86aa7eb45b49d79
SoulJa <superzhaolong@163.com>
2015-06-15 11:32:10 +0800

    添加SJPoto模型

---
+++ SoulJaWeibo/SoulJaWeibo/Classes/Home(首页)/Model/SJPhoto.h
@@ -0,0 +1,13 @@
+//
+//  SJPhoto.h
+//  SoulJaWeibo
+//
+//  Created by SoulJa on 15/6/15.
+//  Copyright (c) 2015年 SoulJa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SJPhoto : NSObject
+
+@end
---
+++ SoulJaWeibo/SoulJaWeibo/Classes/Home(首页)/Model/SJPhoto.m
@@ -0,0 +1,13 @@
+//
+//  SJPhoto.m
+//  SoulJaWeibo
+//
+//  Created by SoulJa on 15/6/15.
+//  Copyright (c) 2015年 SoulJa. All rights reserved.
+//
+
+#import "SJPhoto.h"
+
+@implementation SJPhoto
+
+@end
